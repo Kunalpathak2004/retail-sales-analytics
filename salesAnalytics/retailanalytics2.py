1464bb61c817124cf812637ba8fabe9d9533b796
Kunalpathak2004 <pathakkunal2004@gmail.com>
2025-07-14 22:25:59 +0530

    completed eda and checked null hypothesis, complete!

--- salesAnalytics/retailanalytics2.py
+++ salesAnalytics/retailanalytics2.py
@@ -43,7 +43,7 @@ print(monthly_sales)
 # from this we get a line chart
 # b) using bar and pie chart
 plt.figure(figsize=(12,6))
-sb.lineplot(data=data, x ='Category', y='Sales', hue='Region')
+sb.barplot(data=data, x ='Category', y='Sales', hue='Region')
 plt.title('Category wise Sales by Region')
 plt.show()
 
@@ -51,3 +51,39 @@ region_sales = data.groupby('Region') ['Sales'].sum()
 plt.pie(region_sales,labels=region_sales.index,autopct='%1.1f%%')
 plt.title('Sales by Region')
 plt.show()
+# c) Scatter plot
+plt.figure(figsize=(8,6))
+sb.scatterplot(data=data, x='Sales',y='Profit',hue='Segment')
+plt.title("Sales vs Profit by Customer Segment")
+plt.show()
+
+
+# d) performance analysis
+data.columns = data.columns.str.strip()
+# print(data.columns.tolist())
+product_performance = data.pivot_table(values='Sales',index='Category',columns='Sub-Category',aggfunc='sum')
+plt.figure(figsize=(12,8))
+sb.heatmap(product_performance,cmap='YlGnBu')
+plt.title('Product Performance Heatmap')
+plt.show()
+
+# e) Hypothesis testing and statistical analysis
+region1 = 'East'
+region2 = 'South'
+
+threshold = 0.05
+region1_sales = data[data['Region'] == region1] ['Sales']
+region2_sales = data[data['Region'] == region2] ['Sales']
+t_stat, p_val = stats.ttest_ind(region1_sales, region2_sales)
+print(f'p-value = {p_val}')
+
+if(p_val < threshold):
+    print("Reject null hypothesis, there is significant difference between two regions")
+elif(p_val > threshold):
+    print("Did not reject null hypothesis, there is no significant difference between two regions")
+
+print(data)
+print(num_data.corr())
+print(data.describe())
+
+sb.histplot(data=data, x='Discount', bins=20, kde=True)
